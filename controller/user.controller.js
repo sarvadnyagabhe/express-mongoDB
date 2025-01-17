9cefdbac1d46289ffada0a76efdddf4552c4dbab
sarvadnyagabhe <sarvadnyagabhe@gmail.com>
2025-01-17 13:03:18 +0530

    created get otp api for gmail

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,4 +1,6 @@
-const User = require("../models/user.model");
+const { User } = require("../models/user.model");
+const randomstring = require("randomstring");
+const nodemailer = require("nodemailer");
 
 //create new user
 exports.createUser = async (req, res) => {
@@ -23,6 +25,76 @@ exports.getAllUsers = async (req, res) => {
       message: "Successfully fetch all users",
       data: allUsers,
     });
+  } catch (error) {
+    return res.status(500).json({ success: false, error: error.message });
+  }
+};
+
+//user login
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password, otp } = req.body;
+  } catch (error) {
+    return res.status(500).json({ success: false, error: error.message });
+  }
+};
+
+//send OTP
+exports.sendOTP = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    if (!email) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Email is required" });
+    }
+
+    const randomOtp = randomstring.generate({
+      length: 6,
+      charset: "numeric",
+    });
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587, // Use 587 for TLS or 465 for SSL
+      secure: false, // Use false for TLS, true for SSL
+      auth: {
+        user: "sarvadnyagabhe@gmail.com",
+        pass: "msom flsq jjbj xcau",
+      },
+    });
+
+    const sendOTPEmail = async (email, otp) => {
+      try {
+        const mailOptions = {
+          from: "sarvadnyagabhe@gmail.com",
+          to: email,
+          subject: "OTP for verification",
+          text: `Your OTP code is ${otp}`,
+          html: `<p>Your OTP code is <b>${otp}</b>. It will expire in 5 minutes.</p>`,
+        };
+        const info = await transporter.sendMail(mailOptions);
+        console.log("Email sent:- ", info.response);
+        console.log("otp:- ", otp);
+        return true;
+      } catch (error) {
+        console.error("Error sending email:", error);
+        return false;
+      }
+    };
+
+    const emailSent = await sendOTPEmail(email, randomOtp);
+
+    if (emailSent) {
+      return res
+        .status(200)
+        .json({ success: true, message: "OTP sent successfully" });
+    } else {
+      return res
+        .status(500)
+        .json({ success: false, message: "Failed to send OTP" });
+    }
   } catch (error) {
     return res.status(500).json({ success: false, error: error.message });
   }
--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema(
+//create schema
+const CreateUserSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, trim: true },
     email: {
@@ -14,4 +15,41 @@ const UserSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
-module.exports = mongoose.model("User", UserSchema);
+
+//login schema
+const LoginCreateUserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: { type: String, required: true },
+    otp: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
+
+//otp schema
+const OtpSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    otp: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
+
+// Export Models
+module.exports = {
+  User: mongoose.model("User", CreateUserSchema),
+  LoginUser: mongoose.model("LoginUser", LoginCreateUserSchema),
+  OtpModel: mongoose.model("OtpModel", OtpSchema),
+};
