54dda6e577fe4c4bb0383a88fe7407e625e091f5
sarvadnyagabhe <sarvadnyagabhe@gmail.com>
2025-01-17 12:59:35 +0530

    created api to get product by ID

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const routes = express.Router();
 const ProductController = require("../controller/product.controller");
 
-routes.get("/createProduct", ProductController.createProduct);
+routes.post("/createProduct", ProductController.createProduct);
 routes.get("/getAllProducts", ProductController.getAllProducts);
+routes.get("/getProductById", ProductController.getProductById);
+routes.delete("/deleteProduct/:id", ProductController.deleteProduct);
 
 module.exports = routes;
