162d38ded70f481c70ba3016bcdea1482a559c73
Sarvadnya Gabhe <63846195+sarvadnyagabhe@users.noreply.github.com>
2025-01-17 18:22:12 +0530

    Feature/generate/otp (#2)
    * created get otp api for gmail
    
    * added service file to send otp

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,4 +1,6 @@
-const User = require("../models/user.model");
+const { User } = require("../models/user.model");
+const { otpGenerator } = require("../utils/otpGenerator");
+const { sendOTPEmail } = require("../services/user.otp.service");
 
 //create new user
 exports.createUser = async (req, res) => {
@@ -23,6 +25,52 @@ exports.getAllUsers = async (req, res) => {
       message: "Successfully fetch all users",
       data: allUsers,
     });
+  } catch (error) {
+    return res.status(500).json({ success: false, error: error.message });
+  }
+};
+
+//user login
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password, otp } = req.body;
+  } catch (error) {
+    return res.status(500).json({ success: false, error: error.message });
+  }
+};
+
+//send OTP
+exports.sendOTP = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (!email) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Email is required" });
+    }
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    //utils to generate otp
+    const randomOtp = otpGenerator();
+
+    //service to send otp
+    const emailSent = await sendOTPEmail(email, randomOtp);
+
+    if (emailSent) {
+      return res
+        .status(200)
+        .json({ success: true, message: "OTP sent successfully" });
+    } else {
+      return res
+        .status(500)
+        .json({ success: false, message: "Failed to send OTP" });
+    }
   } catch (error) {
     return res.status(500).json({ success: false, error: error.message });
   }
