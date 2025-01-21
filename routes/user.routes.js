e7f467b2bb4313cd84cde15ea0cef48389dbd7a7
Sarvadnya Gabhe <63846195+sarvadnyagabhe@users.noreply.github.com>
2025-01-21 17:04:25 +0530

    - created user update API (#3)
    - creted login API
    
    Co-authored-by: sarvadnyaInfeon <sarvadnya.gabhe@infeonit.in>

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,5 +6,6 @@ routes.post("/createUser", UserController.createUser);
 routes.get("/getAllUsers", UserController.getAllUsers);
 routes.post("/login", UserController.loginUser);
 routes.post("/sendOtp", UserController.sendOTP);
+routes.put("/updateUser/:id", UserController.updateUser);
 
 module.exports = routes;
